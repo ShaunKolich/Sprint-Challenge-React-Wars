2ab90b02d51ad3e0e5ebdf0d0ec87aceb680de28
ShaunKolich <employmeinc@gmail.com>
2019-06-09 21:06:45 -0400

    Rendered data from server

---
+++ starwars/src/components/StarWarsList.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// // const Chars = (props) => {
+// //     return (
+// //         <div>
+// //             {props.starwarsChars.map((chars) => {
+// //                 return chars = { starwarsChars: data.results };   
+// //             })}          
+      
+// //         </div>
+            
+// //     )
+    
+
+
+
+// // };
+
+// // console.log(Chars);
+// // export default Chars;
